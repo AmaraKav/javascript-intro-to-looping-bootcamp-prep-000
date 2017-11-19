7ae6750087522d68c7f323c6a7d58f23d6521dd2
Kavindu Amarasinghe <amarasinghekavindu@yahoo.com>
2017-11-19 21:52:27 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,7 +17,7 @@ function whileLoop(n){
   return 'done';
 }
 
-function doWhile(array){
+function doWhileLoop(array){
   do{
     array.slice(0, array.length -1);
   } while (array.length > 0 && maybeTrue());
